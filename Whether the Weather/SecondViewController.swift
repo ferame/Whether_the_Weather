6eda20cee8dc5152df9837887ee5c9f424a625d4
ferame <alisauskas.j@gmail.com>
2016-01-30 16:39:57 +0000

    Initial Commit

---
+++ Whether the Weather/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Whether the Weather
+//
+//  Created by Justinas Alisauskas on 30/01/2016.
+//  Copyright © 2016 JustInCode. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
