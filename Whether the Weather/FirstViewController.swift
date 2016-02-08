22d8313459e332c4c3bde0c09a13f5a2abf372b7
ferame <alisauskas.j@gmail.com>
2016-02-08 11:56:26 +0000

    Cozy Weather App
    Upload!

--- Whether the Weather/FirstViewController.swift
+++ Whether the Weather/FirstViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
+var cityList = [String]()
+
 class FirstViewController: UIViewController {
 
+    @IBOutlet var cityTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("ViewDidLoad")
         // Do any additional setup after loading the view, typically from a nib.
+        if NSUserDefaults.standardUserDefaults().objectForKey("cityList") != nil{
+            cityList = NSUserDefaults.standardUserDefaults().objectForKey("cityList") as! [String]
+        }
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(String(cityList.count) + "is the number of rows")
+        print("Number of rows is " + String(cityList.count))
+        return cityList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")//creating cell that we want to return
+        cell.textLabel?.text = cityList[indexPath.row]
+        print("Cell populated with " + String(cityList[indexPath.row]))
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        //method is called whenever user tries to edit item in table
+        //checking for swipe to left
+        if editingStyle == UITableViewCellEditingStyle.Delete{
+            cityList.removeAtIndex(indexPath.row)
+            NSUserDefaults.standardUserDefaults().setObject(cityList, forKey: "cityList")
+            cityTableView.reloadData()
+            print("Item removed")
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        print("ViewDidAppear")
+        cityTableView.reloadData()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
--- Whether the Weather/SecondViewController.swift
+++ Whether the Weather/SecondViewController.swift
@@ -8,10 +8,65 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var userInputField: UITextField!
+    
+    @IBAction func submitButtonPressed(sender: AnyObject) {
+        //let city: String = String(userInputField.text!)
+        let wholeLink = NSURL(string: "http://www.weather-forecast.com/locations/" + userInputField.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        if let url = wholeLink {
+            //url variable(string)
+            //webView.loadRequest(NSURLRequest(URL: url))
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+                //will happen when task completes
+                if let urlContent = data{
+                    
+                    let urlContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                    
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        self.resultLabel.text = self.giveForecast(urlContent!)
+                        self.resultLabel.sizeToFit()
+                        //self.resultLabel.backgroundColor = UIColor.whiteColor()
+                        self.resultLabel.textColor = UIColor.whiteColor()
+                        if !cityList.contains(String(self.userInputField.text!)){
+                            cityList.append(self.userInputField.text!)
+                            print("CityList appended " + String(self.userInputField.text!))
+                            NSUserDefaults.standardUserDefaults().setObject(cityList, forKey: "cityList")
+                        }
+                    })
+
+                }else{
+                    //Show error message
+                    
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        self.resultLabel.text = "City not found/ Information not available"
+                        self.resultLabel.sizeToFit()
+                        self.resultLabel.textColor = UIColor.redColor()
+                    })
+
+                }
+            }
+            task.resume()
+        }else{
+            
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.resultLabel.text = "City not found/ Information not available"
+                self.resultLabel.sizeToFit()
+            })
+            
+        }
+    }
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.userInputField.delegate = self
+        //resultLabel.sizeToFit()
+        print("Second View Did Load")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +75,29 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+        print("touches around the screen")
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        print("return button touched")
+        return true
+    }
+    
+    func giveForecast(urlContent: NSString) -> String{
+        var forecast: String
+        if urlContent.containsString("phrase\">"){
+            print("passed forecast")
+            var arrayForecast = urlContent.componentsSeparatedByString("phrase\">")
+            arrayForecast = arrayForecast[1].componentsSeparatedByString("</span>")
+            forecast = arrayForecast[0].stringByReplacingOccurrencesOfString("&deg;", withString: "°")
+            
+        }else{
+            print("gotcha")
+            forecast = "City not found/ Information not available"
+        }
+        return forecast
+    }
 }
-
